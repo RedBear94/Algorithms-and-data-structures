dc8db7cd1a51d58023ba7a1874e84ea02daca8b4
Pavel <redbear95@gmail.com>
2020-11-14 14:12:10 +0300

    update hw5

--- HomeWork/src/main/java/hw4/Exponentiation.java
+++ HomeWork/src/main/java/hw4/Exponentiation.java
@@ -2,19 +2,26 @@ package hw4;
 
 public class Exponentiation {
     public static void main(String[] args) {
-        System.out.println(exp(2, 10));
+        System.out.println(exp(2, 9));
+        System.out.println(exp(2, -2));
+        //System.out.println(exp(0, 0));
+        System.out.println(exp(-3, 3));
+        System.out.println(exp(1, 3));
     }
 
-    private static int exp(int a, int n) {
-        return exponentiation(a, n, a);
-    }
-
-    private static int exponentiation(int a, int n, int t) {
-        if(n == 0) {
+    private static double exp(double a, int n) {
+        if(n == 0 && a != 0) {
             return 1;
+        } else if(a == 0 && n == 0){
+            throw new IllegalArgumentException("Invalid a " + a + " for n = 0");
         } else if(n == 1){
             return a;
         }
-        return exponentiation(a * t, n - 1, t);
+
+        if(n > 0) {
+            return a * exp(a, n - 1);
+        } else {
+            return 1/(a * exp(a, -n - 1));
+        }
     }
 }
